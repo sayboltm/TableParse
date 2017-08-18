b26fd83488b50f10de002f31eca114da47dd74a6
sayboltm <sayboltm@users.noreply.github.com>
2017-08-18 17:09:52 -0400

    Added more examples, these actually work

---
+++ test3_py3.py
@@ -0,0 +1,32 @@
+#https://stackoverflow.com/questions/6690367/parse-extract-table-data-using-python
+#import HTMLParser
+import html.parser
+
+data = '''
+<html> 
+<table border="1px"> 
+<tr>
+<td>yes</td>
+<td>no</td>
+</tr>
+</table>
+</html>
+'''
+class TableParser(html.parser.HTMLParser):
+    def __init__(self):
+        html.parser.HTMLParser.__init__(self)
+        self.in_td = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'td':
+            self.in_td = True
+
+    def handle_data(self, data):
+        if self.in_td:
+            print(data)
+
+    def handle_endtag(self, tag):
+        self.in_td = False
+
+p = TableParser()
+p.feed(data)
