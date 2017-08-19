461c892b2b15463e5ff479662796d7e00444165e
sayboltm <sayboltm@users.noreply.github.com>
2017-08-19 10:22:28 -0400

    Table successfully pulled into python lists!

--- main.py
+++ main.py
@@ -25,21 +25,34 @@ class TableParser(html.parser.HTMLParser):
     def __init__(self):
         html.parser.HTMLParser.__init__(self)
         self.in_td = False
+        self.in_tr = False
+        self.rowlist = []
+        self.datalist = []
 
     def handle_starttag(self, tag, attrs):
+        if tag == 'tr':
+            self.in_tr = True
         if tag == 'td':
             self.in_td = True
 
     def handle_data(self, data):
+#        if self.in_td:
+#            print(data)
         if self.in_td:
-            print(data)
+            self.datalist.append(data)
+
 
     def handle_endtag(self, tag):
         self.in_td = False
+        self.in_tr = False
+        if tag == 'tr':
+            self.rowlist.append(self.datalist)
+            self.datalist = [] # Reset the datalist
     
     def unknown_decl(self, data):
         print('[-] WARNING: Unknown declaration! Aborting for debug purposes :) ')
         sys.exit(1)
+
 
 p = TableParser()
 #p.feed(testdata2)
@@ -48,3 +61,4 @@ htmlfile = 'junematch.html'
 #htmlfile = '../../CZ3QFP/VINparse/testpage.html'
 with open(htmlfile, 'r') as infile:
     p.feed(infile.read())
+q = p.rowlist # This is the table in pythonic form!
